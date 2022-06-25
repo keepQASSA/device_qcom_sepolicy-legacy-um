0ea27d9587efeb332fe8eaa6e5bc5317119bed2f
ghou8s <ghou8s@gmail.com>
2022-06-25 19:14:19 +0000

    qassa [8/22]
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- sepolicy.mk
+++ sepolicy.mk
@@ -27,4 +27,4 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy-legacy-um/private
 
--include device/aosqp/sepolicy/qcom/sepolicy.mk
+-include device/qassa/sepolicy/qcom/sepolicy.mk
